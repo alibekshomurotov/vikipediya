1baa65db3477eeffc29bc0fbf2496c01db4f5455
Shomurotov Alibek <umidbekshomurotov3@gmail.com>
2026-05-15 10:29:08 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/vikipediya/', // Repository nomingizga moslashtiring!
+  base: './',
   server: {
     port: 3000,
     open: true
@@ -12,7 +12,7 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
-    minify: 'terser',
+    minify: 'esbuild', // terser o'rniga esbuild (Vite default)
     rollupOptions: {
       output: {
         manualChunks: {
